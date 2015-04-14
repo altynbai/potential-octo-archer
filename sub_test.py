1d7ce94131c367779d1b6892d51588a7da91fb66
altynbai <altynbai@users.noreply.github.com>
2015-04-14 20:46:42 +0600

    added class NetworkDevice

--- sub_test.py
+++ sub_test.py
@@ -2,3 +2,10 @@ import os
 import sys
 import shutils
 
+def class NetworkDevice(object):
+  def __init__(self, ip, username, password):
+    self.ip = ip
+    self.username = username
+    self.password = password
+    
+
